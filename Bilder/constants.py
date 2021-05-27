bb8b07f35f7f3ba63c158170a6d651766ace4529
Jonathannd03 <82469575+Jonathannd03@users.noreply.github.com>
2021-05-27 10:03:31 +0200

    Add files via upload

---
+++ Bilder/constants.py
@@ -0,0 +1,18 @@
+import pygame
+
+"""wir geben die Breite und Höhe des Spielfelds an"""
+WIDTH, HEIGHT = 600, 600
+
+"""Wir geben die Anzahl der Spalten und Zeilen unseres Schachbretts an """
+ROWS, COLS = 6, 6
+
+"""Wir geben wir groß ein Quadrat Schachbretts ist an"""
+SQUARE_SIZE = WIDTH // COLS
+
+"""Wir definieren einige der Farbcodes, die wir verwenden werden"""
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
+YELLOW = (255, 255, 0)
